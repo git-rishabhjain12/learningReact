b0baf0e8b3e35f821768c587809534c332a2eb7f
rishabhjain12 <rishabhj.1222@gmail.com>
2023-04-19 13:06:48 +0530

    added delete functionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
+// import DoneIcon from '@mui/icons-material/Done';
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Todo() {
   const [inputValue, setValue] = useState("");
   const [inputValues, setValues] = useState([]);
 
   function handleChange(e) {
-    setValue(e.target.value)
+    setValue(e.target.value);
   }
 
   function handleSumbit(e) {
     e.preventDefault();
     setValues([...inputValues, inputValue]);
     setValue("");
+  }
+
+  function handleDeletion(itemToDelete) {
+    // console.log(itemToDelete);
+    setValues(
+      inputValues.filter((value, index) => {
+        return index !== itemToDelete;
+      })
+    );
   }
 
   return (
@@ -21,14 +32,23 @@ function Todo() {
         <input
           type="text"
           placeholder="Enter Your Name"
+          autoFocus
           value={inputValue}
           onChange={handleChange}
         />
-        <button type="sumbit">Add Task</button>
+        <button type="submit">Add Task</button>
       </form>
       <ul>
         {inputValues.map((value, index) => {
-          return <li key={index}>{value}</li>;
+          return (
+            <li key={index}>
+              {value}
+              <DeleteIcon
+                className="deleteBtn"
+                onClick={() => handleDeletion(index)}
+              />
+            </li>
+          );
         })}
       </ul>
     </>
