176bfa92b133b5b73d1c741d29bfaf7a8fd010f2
rishabhjain12 <rishabhj.1222@gmail.com>
2023-05-02 11:27:16 +0530

    nested routing added

--- src/Routing/Home.js
+++ src/Routing/Home.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Main from "./Main";
 import About from "./About";
+import BlogParent from "./BlogParent";
 import Blog from "./Blog";
-import Contact from "./Contact";
 import SingleBlog from "./SingleBlog";
+import Contact from "./Contact";
+import NotFound from "./NotFound";
 
 function Home() {
   return (
@@ -14,9 +16,12 @@ function Home() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<SingleBlog />} />
+        <Route path="/blog" element={<BlogParent />}>
+          <Route index element={<Blog />} />
+          <Route path=":id" element={<SingleBlog />} />
+        </Route>
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
